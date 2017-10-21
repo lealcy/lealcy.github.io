774288a623242cb26bbfa87962eeab31a89355dd
Lealcy Tardelli Belegante <lealcy@gmail.com>
2017-10-21 11:23:52 -0200

    stranded-idle: Mining Drills!!!!

--- stranded-idle/items.js
+++ stranded-idle/items.js
@@ -6,14 +6,14 @@ const itemData = {
         name: "Coal",
         image: "coal",
         craftable: true,
-        productionTime: { /*burnerMiningDrill: 3571.4, eletricMiningDrill: 1904.7*/ },
+        productionTime: { burnerMiningDrill: 3571.4 },
         products: { coal: 1 },
     },
     stone: {
         name: "Stone",
         image: "stone",
         craftable: true,
-        /*    producedBy: { burnerMiningDrill: 2721, eletricMiningDrill: 1538.4 },*/
+        productionTime: { burnerMiningDrill: 2721 },
         products: { stone: 1 },
     },
     iron: {
@@ -21,12 +21,14 @@ const itemData = {
         craftable: true,
         products: { iron: 1 },
         image: "iron",
+        productionTime: { burnerMiningDrill: 3571.4 },
     },
     copper: {
         name: "Copper Ore",
         craftable: true,
         products: { copper: 1 },
         image: "copper",
+        productionTime: { burnerMiningDrill: 3571.4 },
     },
 
     // Intermediate Products
@@ -58,26 +60,30 @@ const itemData = {
         cost: { ironPlate: 5 },
         productionTime: { stoneFurnace: 17543.8, },
     },
-
-    /*
-       steelPlate: {
-           name: "Steel Plate",
-       },
-       ironGear: {
-           name: "Iron Gear",
-           craftable: true,
-           cost: { ironPlate: 2 }
-       },
-       copperCable: {
-           name: "Copper Cable",
-           craftable: true,
-           cost: { copperPlate: 1 },
-       },
-       eletronicCircuit: {
-           name: "Eletronic Circuit",
-           craftable: true,
-           cost: { copperCable: 3, ironPlate: 1 },
-       },*/
+    ironGear: {
+        name: "Iron Gear",
+        image: "ironGear",
+        craftable: true,
+        cost: { ironPlate: 2 },
+        products: { ironGear: 1 },
+        productionTime: { assembler1: 500 },
+    },
+    copperCable: {
+        name: "Copper Cable",
+        image: "copperCable",
+        craftable: true,
+        cost: { copperPlate: 1 },
+        products: { copperCable: 1 },
+        productionTime: { assembler1: 500 },
+    },
+    eletronicCircuit: {
+        name: "Eletronic Circuit",
+        image: "eletronicCircuit",
+        craftable: true,
+        cost: { copperCable: 3, ironPlate: 1 },
+        products: { eletronicCircuit: 1 },
+        productionTime: { assembler1: 500 },
+    },
     stoneFurnace: {
         name: "Stone Furnace",
         craftable: true,
@@ -86,21 +92,27 @@ const itemData = {
         consume: { coal: 0.08035 },
         productionFactor: 7,
         products: { stoneFurnace: 1 },
+        productionTime: { assembler1: 500 },
     },
-    /*   burnerMiningDrill: {
-           name: "Burner Mining Drill",
-           craftable: true,
-           cost: { ironGear: 3, ironPlate: 3, stoneFurnace: 1 },
-           image: "burnerMiningDrill",
-           category: "miner",
-       },
-       assembler1: {
-           name: "Assembler 1",
-           craftable: true,
-           cost: { eletronicCircuit: 3, ironGear: 5, ironPlate: 9 },
-           image: "assembler1",
-           category: "assembler",
-       },
+    burnerMiningDrill: {
+        name: "Burner Mining Drill",
+        image: "burnerMiningDrill",
+        craftable: true,
+        cost: { ironGear: 3, ironPlate: 3, stoneFurnace: 1 },
+        consume: { coal: 0.14285 },
+        image: "burnerMiningDrill",
+        category: "miner",
+        products: { burnerMiningDrill: 1 },
+        productionTime: { assembler1: 500 },
+    },
+    assembler1: {
+        name: "Assembler 1",
+        craftable: true,
+        cost: { eletronicCircuit: 3, ironGear: 5, ironPlate: 9 },
+        image: "assembler1",
+        products: { assembler1: 1 },
+        productionTime: { assembler1: 500 },
+    },/*
    
        // Machines
    
