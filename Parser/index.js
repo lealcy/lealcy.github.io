f68ea4b08506609960a6adceed92ddfbc088d6d0
Lealcy Tardelli Belegante <lealcy@gmail.com>
2018-02-09 19:00:40 -0200

    add vars + addiction

--- Parser/index.js
+++ Parser/index.js
@@ -19,31 +19,40 @@ let refresh = false;
 let jump = null;
 let stop = false;
 const labels = new Map;
+const vars = new Map;
 
 const instructions = new Map;
-instructions.set(/^\s*canvas\s+(\d+)\s*,\s*(\d+)\s*$/i, r => {
-    canvas.width = r[1];
-    canvas.height = r[2];
+instructions.set(/^\s*([a-zA-Z_][a-zA-Z0-9_]*)\s*=\s*(.*)\s*/i, r => {
+    vars.set(r[1], solve(r[2]));
+});
+instructions.set(/^\s*canvas\s+(\d+|[a-zA-Z_][a-zA-Z0-9_]*)\s*,\s*(\d+|[a-zA-Z_][a-zA-Z0-9_]*)\s*$/i, r => {
+    canvas.width = solve(r[1]);
+    canvas.height = solve(r[2]);
 });
 instructions.set(/^\s*color\s+([a-zA-Z]+)\s*$/i, r => {
     context.fillStyle = r[1];
 });
-instructions.set(/^\s*text\s+"([^"]*)"(?:\s*,\s*(\d+))?\s*$/i, r => {
-    context.fillText(r[1], 0, 0, r[2]);
+instructions.set(/^\s*text\s+(\d+|"[^"]*"|[a-zA-Z_][a-zA-Z0-9_]*)(?:\s*,\s*(\d+|[a-zA-Z_][a-zA-Z0-9_]*))?\s*$/i, r => {
+    context.fillText(solve(r[1]), 0, 0, solve(r[2]));
 });
-instructions.set(/^\s*text\s+(\d+)\s*,\s*(\d+)\s*,\s*"([^"]*)"(?:\s*,\s*(\d+))?\s*$/i, r => {
-    context.fillText(r[3], r[1], r[2], r[4]);
+instructions.set(/^\s*text\s+(\d+|[a-zA-Z_][a-zA-Z0-9_]*)\s*,\s*(\d+|[a-zA-Z_][a-zA-Z0-9_]*)\s*,\s*(\d+|"[^"]*"|[a-zA-Z_][a-zA-Z0-9_]*)(?:\s*,\s*(\d+|[a-zA-Z_][a-zA-Z0-9_]*))?\s*$/i, r => {
+    context.fillText(r[3], solve(r[1]), solve(r[2]), solve(r[4]));
 });
 instructions.set(/^\s*refresh\s*$/i, r => {
     refresh = true;
 });
-instructions.set(/^\s*goto\s+([^\s]+)\s*/i, r => {
-    jump = r[1];
+instructions.set(/^\s*goto\s+(\d+|[a-zA-Z_][a-zA-Z0-9_]*)\s*/i, r => {
+    jump = solve(r[1]);
 });
-instructions.set(/^\s*translate\s+(\d+)\s*,\s*(\d+)\s*$/i, r => {
-    context.translate(r[1], r[2]);
+instructions.set(/^\s*translate\s+(\d+|[a-zA-Z_][a-zA-Z0-9_]*)\s*,\s*(\d+|[a-zA-Z_][a-zA-Z0-9_]*)\s*$/i, r => {
+    context.translate(solve(r[1]), solve(r[2]));
 });
 instructions.set(/^\s*clear\s*$/i, r => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+});
+instructions.set(/^\s*reset\s*$/i, r => {
+    context.setTransform(1, 0, 0, 1, 0, 0);
+    context.fillStyle = "white";
     context.clearRect(0, 0, canvas.width, canvas.height);
 });
 
@@ -63,14 +72,14 @@ function parseLine() {
     let text = lines[line];
     const label = text.match(/^\s*(\d+)\s+/);
     if (label) {
-        console.log(`Label "${label[1]}" -> ${line}`);
-        labels.set(label[1], line);
+        //console.log(`Label "${label[1]}" -> ${line}`);
+        labels.set(parseInt(label[1]), line);
         text = text.substring(label[0].length);
     }
     for (const [regex, fn] of instructions) {
         const result = text.match(regex);
         if (result) {
-            console.log(result);
+            //console.log(result);
             fn(result);
             break;
         }
@@ -81,6 +90,27 @@ function parseLine() {
     } else {
         line++;
     }
+}
+
+function solve(x) {
+    if (x === undefined) {
+        return;
+    }
+    x = x.trim();
+    if (/^\d+$/.test(x)) {
+        return parseInt(x);
+    }
+    if (/^".*"$/.test(x)) {
+        return x.slice(1, -1);
+    }
+    if (/^[a-zA-Z_][a-zA-Z0-9_]*$/.test(x)) {
+        return vars.get(x);
+    }
+    let r;
+    if (r = x.match(/^(.+)\+(.+)$/)) {
+        return solve(r[1]) + solve(r[2]);
+    }
+    console.log("fail to solve X", x);
 }
 
 document.querySelector("button#parse").addEventListener("click", e => {
