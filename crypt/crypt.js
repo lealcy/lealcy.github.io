eb4d5478c346414bf7f7b0e4e09836c8a8a307cd
Lealcy Tardelli Belegante <lealcy@gmail.com>
2019-02-05 14:04:13 -0200

    new version!

--- crypt/crypt.js
+++ crypt/crypt.js
@@ -1,53 +1,33 @@
 "use strict";
 
-function enc(text, pass) {
-    const hashPass = hashCode(pass);
-    let rand, dict, cypher;
-    do {
-        rand = new SeededRandom(hashPass);
-        dict = generateSubstitutionDictionary(rand);
-        cypher = "";
-        for (let i = 0; i < text.length; i++) {
-            const dec = text.charCodeAt(i);
-            console.log(dec);
-            cypher += dict[dec][Math.floor(Math.random() * dict[dec].length)];
-        }
-    } while (dec(cypher, pass) !== text);
-    return cypher;
-}
-
-function dec(cypher, pass) {
-    const hashPass = hashCode(pass);
-    const rand = new SeededRandom(hashPass);
-    const dict = generateSubstitutionDictionary(rand);
-    dict.forEach((arr, i) => {
-        arr.forEach(v => {
-            cypher = cypher.replace(v, String.fromCharCode(i));
-        });
-    });
-    return cypher;
-}
-
-
-function generateSubstitutionDictionary(rand) {
-    const map = [];
-    for (let i = 32; i < 256; i++) {
-        map[i] = [];
-        for (let j = 0; j < 100; j++) {
-            map[i].push(fingerprint(rand));
-        }
+function enc(text, cypherPool) {
+    let cypher = "";
+    for (let i = 0; i < text.length; i++) {
+        cypher += cypherPool.getCharKey(text.charCodeAt(i));
     }
-    return map;
+    return btoa(cypher);
 }
 
-function fingerprint(rand) {
-    const pool = "1234567890-=!@#$%&*()_+qwertyuiop´[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP`{ASDFGHJKLÇ^}|ZXCVBNM<>:?";
-    const len = Math.round(rand.next() * 6 + 3);
-    let fp = "";
-    for (let i = 0; i < len; i++) {
-        fp += pool[Math.floor(rand.next() * pool.length)];
+function dec(cypher, cypherPool) {
+    const bcypher = atob(cypher);
+    let text = "";
+
+    let p = 0;
+    next: while (p < bcypher.length) {
+        for (let keySize = cypherPool.minKeySize; keySize <= cypherPool.maxKeySize; keySize++) {
+            const key = bcypher.substr(p, keySize);
+            let charCode = cypherPool.getChar(key);
+            if (charCode === null) {
+                continue;
+            }
+            text += String.fromCharCode(charCode);
+            p += keySize;
+            continue next;
+        }
+        throw new Error("not found!");
+        break;
     }
-    return fp;
+    return text;
 }
 
 function hashCode(str) {
@@ -62,11 +42,6 @@ function hashCode(str) {
     return hash;
 };
 
-
-function randMinMax(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-};
-
 class SeededRandom {
     constructor(seed = Math.random()) {
         this.seed = seed;
@@ -75,5 +50,74 @@ class SeededRandom {
         this.seed = Math.sin(this.seed) * 10000;
         return this.seed - Math.floor(this.seed);
 
+    }
+    nextIntBetween(min, max) {
+        return Math.floor(this.next() * (max - min)) + min;
+    }
+}
+
+class CypherPool {
+    constructor(seed, firstCharCode, lastCharCode, nKeysPerChar, minKeySize, maxKeySize) {
+        const now = performance.now();
+        this.rand = new SeededRandom(seed);
+        this.firstCharCode = firstCharCode;
+        this.lastCharCode = lastCharCode;
+        this.nKeysPerChar = nKeysPerChar;
+        this.minKeySize = minKeySize;
+        this.maxKeySize = maxKeySize;
+        this.poolSize = (lastCharCode - firstCharCode) * nKeysPerChar;
+
+        // Generate size map
+        this.sizeMap = new Int8Array(this.poolSize);
+        this.totalSize = 0;
+        for (let i = 0; i < this.poolSize; i++) {
+            let size = this.rand.nextIntBetween(minKeySize, maxKeySize + 1);
+            this.totalSize += size;
+            this.sizeMap[i] = size;
+        }
+        this.pool = new Uint8Array(this.totalSize);
+        for (let i = 0; i < this.totalSize; i++) {
+            this.pool[i] = Math.floor(this.rand.next() * 256);
+        }
+    }
+
+    getCharKey(charCode) {
+        let keyIndex = Math.floor(Math.random() * this.nKeysPerChar);
+        let sizeMapOffset = ((charCode - this.firstCharCode) * this.nKeysPerChar) + keyIndex;
+        let keySize = this.sizeMap[sizeMapOffset];
+        let keyOffset = 0;
+
+        for (let i = 0; i < sizeMapOffset; i++) {
+            keyOffset += this.sizeMap[i];
+        }
+
+        let key = "";
+        for (let i = keyOffset; i < keyOffset + keySize; i++) {
+            key += String.fromCharCode(this.pool[i]);
+        }
+
+        return key;
+    }
+
+    getChar(key) {
+        const keyMap = new Uint8Array(key.split("").map(v => v.charCodeAt(0)));
+        const keyMapSize = keyMap.length;
+        let poolOffset = 0;
+        const sizeMapSize = this.sizeMap.length;
+        search: for (let sizeMapIndex = 0; sizeMapIndex < sizeMapSize; sizeMapIndex++) {
+            if (this.sizeMap[sizeMapIndex] !== keyMapSize) {
+                poolOffset += this.sizeMap[sizeMapIndex];
+                continue search;
+            }
+            for (let i = 0; i < keyMapSize; i++) {
+                if (this.pool[poolOffset + i] !== keyMap[i]) {
+                    poolOffset += this.sizeMap[sizeMapIndex];
+                    continue search;
+                }
+            }
+            const char = Math.floor(sizeMapIndex / this.nKeysPerChar) + this.firstCharCode
+            return char;
+        }
+        return null;
     }
 }
--- crypt/main.js
+++ crypt/main.js
@@ -1,10 +1,35 @@
 "use strict";
 
+const FIRST_CHAR = 32;
+const LAST_CHAR = 256;
+const MIN_KEY_SIZE = 3;
+const MAX_KEY_SIZE = 7;
+const KEYS_PER_CHAR = 100; // 100;
+
+
+let cypherPool;
+
+const dictBtnEl = document.getElementById("dict");
 const srcEl = document.getElementById("src");
 const dstEl = document.getElementById("dst");
 const encryptBtnEl = document.getElementById("encrypt");
 const decryptBtnEl = document.getElementById("decrypt");
 const passEl = document.getElementById("pass");
 
-encryptBtnEl.addEventListener("click", e => dstEl.value = enc(srcEl.value, pass.value));
-decryptBtnEl.addEventListener("click", e => srcEl.value = dec(dstEl.value, pass.value));
+dictBtnEl.addEventListener("click", e => {
+    encryptBtnEl.setAttribute("disabled", "disabled");
+    decryptBtnEl.setAttribute("disabled", "disabled");
+    if (pass.value === "") {
+        return;
+    }
+    dictBtnEl.setAttribute("disabled", "disabled");
+    setTimeout(() => {
+        cypherPool = new CypherPool(hashCode(pass.value), FIRST_CHAR, LAST_CHAR, KEYS_PER_CHAR, MIN_KEY_SIZE, MAX_KEY_SIZE);
+        encryptBtnEl.removeAttribute("disabled");
+        decryptBtnEl.removeAttribute("disabled");
+        dictBtnEl.removeAttribute("disabled");
+    }, 1);
+
+});
+encryptBtnEl.addEventListener("click", e => dstEl.value = enc(srcEl.value, cypherPool));
+decryptBtnEl.addEventListener("click", e => srcEl.value = dec(dstEl.value, cypherPool));
