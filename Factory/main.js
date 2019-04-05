4b3f180653ad178f8065a637f4f35461401947ec
Lealcy Tardelli Belegante <lealcy@gmail.com>
2019-04-05 17:28:57 -0300

    Update main.js

--- Factory/main.js
+++ Factory/main.js
@@ -2,7 +2,11 @@
 
 class Application {
     constructor() {
-        this._mainWindow = new Window();
+        this._mainWindow = new Window("Unnamed Window");
+    }
+    
+    run() {
+        document.body.appendChild(this._mainWindow.generateHTML());
     }
     
     get mainWindow() { return this._mainWindow; }
@@ -13,4 +17,15 @@ class Window {
     constructor(title = "") {
         this._title = title;
     }
+    
+    generateHTML() {
+        const div = document.createElement("div");
+        const title = document.createElement("h1");
+        title.textContent = this._title;
+        div.appendChild(title);
+        return div;
+    }
 }
+
+const app = new Application();
+app.run();
