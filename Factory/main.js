d7628f7539f3f72170d6785f0ccfaf27f64ad212
Lealcy Tardelli Belegante <lealcy@gmail.com>
2019-04-05 16:56:13 -0300

    Update main.js

--- Factory/main.js
+++ Factory/main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+class Application {
+    constructor() {
+        this._mainWindow = null;
+    }
+    
+    get mainWindow() { return this._mainWindow; }
+    set mainWindow(window) { this._mainWindow = window instanceof Window ? window : null; }
+}
+
+class Window {
+    constructor(title = "") {
+        this._title = title;
+    }
+}
