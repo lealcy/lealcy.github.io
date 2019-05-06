f662bd528c0f25b3308d6742ae74d72fb7fd08fc
Lealcy Tardelli Belegante <lealcy@gmail.com>
2019-05-06 11:32:17 -0300

    Create main.js

---
+++ Points/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const screen = getElementById("screen");
+const context = screen.getContext("2d", { alpha: false });
+
+class Point {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius;
+    }
+
+    draw(context) {
+        context.fillStyle = "blue";
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        context.fill();
+    }
+}
+
+const points = []
+
+function start() {
+    requestAnimationFrame(update);
+}
+
+function update() {
+    requestAnimationFrame(update);
+    points.forEach(point => point.draw());
+}
