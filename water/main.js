53d01221ccd63b78c56252db6ee8a7537bfc06b0
Lealcy Belegante Junior <lealcy@gmail.com>
2019-09-13 09:12:50 -0300

    Add files via upload

--- water/main.js
+++ water/main.js
@@ -1,70 +1,68 @@
 "use strict";
 
 const canvas = document.querySelector("canvas");
-const context = canvas.getContext("2d");
-context.imageSmoothingEnabled = false;
-
 const SCALE = 10;
 const WIDTH = Math.floor(canvas.width / SCALE);
 const HEIGHT = Math.floor(canvas.height / SCALE);
-let stopped = true;
-let textSize = 0;
-let state = Array(WIDTH).fill(0).map(x => Array(HEIGHT).fill(0));
-
-let mousex = 0;
-let mousey = 0;
-let mousebutton = -1;
-canvas.addEventListener("mousedown", e => mousebutton = e.button, false);
-canvas.addEventListener("mouseup", e => mousebutton = -1, false);
-canvas.addEventListener("mousemove", mousemove, false);
-canvas.addEventListener("contextmenu", e => e.preventDefault(), false);
+const context = canvas.getContext("2d");
 context.scale(SCALE, SCALE);
+context.imageSmoothingEnabled = false;
 
-function mousemove(e) {
-    mousex = Math.floor(e.offsetX / SCALE);
-    mousey = Math.floor(e.offsetY / SCALE);
-}
-
-function update(ts) {
-    requestAnimationFrame(update);
-    if (mousebutton !== -1) {
-        stop();
-        //console.log("click", mousebutton, mousex, mousey);
-        //get(state[mousex,  mousey)] = get(state[mousex,  mousey)] ? 0 : 1;
-        if (mousebutton === 0) {
-            state[mousex][mousey] = 1;
-        } else {
-            state[mousex][mousey] = 0;
-        }
-
+class State {
+    constructor() {
+        this.state = this._createEmptyState();
     }
-    if (!stopped) {
-        const newState = Array(WIDTH).fill(0).map(x => Array(HEIGHT).fill(0));
-        const offset = textSize < 4 ? 18 : 30 - (textSize * 3);
-        const limit = textSize < 4 ? 42 : Math.min(WIDTH, offset + textSize * 6);
+
+    get(x, y) {
+        if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
+            return 0;
+        }
+        return this.state[x][y];
+    }
+
+    set(x, y, value) {
+        this.state[x][y] = value;
+    }
+
+    iterate(offset = 0, limit = WIDTH) {
+        const newState = this._createEmptyState();
         for (let x = offset; x < limit; x++) {
             for (let y = 0; y < HEIGHT; y++) {
-                const livingNeighbours = get(x, y - 1) + get(x + 1, y - 1) + get(x + 1, y) + get(x + 1, y + 1) +
-                    get(x, y + 1) + get(x - 1, y) + get(x - 1, y + 1) + get(x - 1, y - 1);
-                if (get(x, y) === 0 && livingNeighbours === 3) {
+                const livingNeighbours = this.get(x, y - 1) + this.get(x + 1, y - 1) + this.get(x + 1, y) + this.get(x + 1, y + 1) +
+                    this.get(x, y + 1) + this.get(x - 1, y) + this.get(x - 1, y + 1) + this.get(x - 1, y - 1);
+                if (this.get(x, y) === 0 && livingNeighbours === 3) {
                     newState[x][y] = 1;
-                } else if (get(x, y) === 1 && livingNeighbours >= 1 && livingNeighbours <= 4) {
+                } else if (this.get(x, y) === 1 && livingNeighbours >= 1 && livingNeighbours <= 4) {
                     newState[x][y] = 1;
                 } else {
-                    newState[x][y] = 0
+                    newState[x][y] = 0;
                 }
             }
         }
-        state = newState;
+        this.state = newState;
     }
 
-    //fluid();
+    _createEmptyState() {
+        return Array(WIDTH).fill(0).map(x => Array(HEIGHT).fill(0));
+    }
+}
+
+let state = new State;
+let textSize = 0;
+
+function update(ts) {
+    requestAnimationFrame(update);
+
+    const offset = textSize < 4 ? 18 : 30 - (textSize * 3);
+    const limit = textSize < 4 ? 42 : Math.min(WIDTH, offset + textSize * 6);
+    state.iterate(offset, limit);
+
     context.save();
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = "black";
     for (let x = 0; x < WIDTH; x++) {
         for (let y = 0; y < HEIGHT; y++) {
-            if (get(x, y)) {
+            if (state.get(x, y)) {
                 context.fillRect(x, y, 1, 1);
             }
         }
@@ -72,28 +70,8 @@ function update(ts) {
     context.restore();
 }
 
-function get(x, y) {
-    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT) {
-        return 0;
-    }
-    return state[x][y];
-}
-
-function start() {
-    stopped = false;
-}
-
-function stop() {
-    stopped = true;
-}
-
-function clean() {
-    state = Array(WIDTH).fill(0).map(x => Array(HEIGHT).fill(0));
-    start();
-}
-
-function paste() {
-    clean();
+function onInput() {
+    state = new State;
     let text = document.querySelector("input").value.toLocaleLowerCase().match(/[a-z ]/g);
     if (text === null) {
         return;
@@ -110,7 +88,7 @@ function paste() {
         const offset = i * 6 + (30 - textSize * 3);
         arr.forEach(line => {
             for (let x = 0; x < 6; x++) {
-                state[offset + x][y] = line[x];
+                state.set(offset + x, y, line[x]);
             }
             y++;
         });
@@ -121,12 +99,11 @@ function dump() {
     const arr = Array(6).fill(0).map(x => Array(6).fill(0));
     for (let x = 0; x < 6; x++) {
         for (let y = 0; y < 6; y++) {
-            arr[x][y] = state[y][x];
+            arr[x][y] = state.get(y, x);
         }
     }
     console.log(JSON.stringify(arr));
 }
-
 
 update();
 
